4ad9e3dbf3fadc67cd9a8a07cac0538661a9c0a6
omar <omaraldyb2020@gmail.com>
2026-07-21 23:16:53 +0300

    first

---
+++ backend/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+app.listen(3001, function () { return console.log('listening on port 3001 '); });
